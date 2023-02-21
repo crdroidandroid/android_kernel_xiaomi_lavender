77fa9a8a767ae44ce39dd3294a7ecdaceb047967
ImSpiDy <SpiDy2713@gmail.com>
2023-02-21 21:18:57 +0000

    cpufreq: blu_schedutil: tune default rate limit us
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- kernel/sched/cpufreq_blu_schedutil.c
+++ kernel/sched/cpufreq_blu_schedutil.c
@@ -707,8 +707,8 @@ static int sugov_init(struct cpufreq_policy *policy)
 		goto stop_kthread;
 	}
 
-	tunables->up_rate_limit_us = LATENCY_MULTIPLIER;
-	tunables->down_rate_limit_us = LATENCY_MULTIPLIER;
+	tunables->up_rate_limit_us = LATENCY_MULTIPLIER * 5;
+	tunables->down_rate_limit_us = LATENCY_MULTIPLIER * 5;
 	lat = policy->cpuinfo.transition_latency / NSEC_PER_USEC;
 	if (lat) {
 		tunables->up_rate_limit_us *= lat;
